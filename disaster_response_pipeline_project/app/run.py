d1146582ccc76400260d836ad2e958e779c10d59
Lucas Saffi <lucassaffi@hotmail.com>
2021-03-07 14:33:56 -0300

    Add files via upload

--- disaster_response_pipeline_project/app/run.py
+++ disaster_response_pipeline_project/app/run.py
@@ -11,7 +11,6 @@ from plotly.graph_objs import Bar
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
-
 app = Flask(__name__)
 
 def tokenize(text):
@@ -26,11 +25,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/MySQLdb.db')
+df = pd.read_sql_table('MySQL', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/MyModel.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -42,6 +41,11 @@ def index():
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
+    
+    df_copy = df.copy()
+    df_copy['message_count'] = df_copy['message'].apply(lambda x: len(x.split()))
+    genre_len_mean = df_copy.groupby('genre').mean()['message_count']
+    print(genre_len_mean)
     
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
@@ -66,12 +70,37 @@ def index():
         }
     ]
     
+    graphs_2 = [
+        {
+            'data': [
+                Bar(
+                    x=genre_names,
+                    y=genre_len_mean
+                )
+            ],
+
+            'layout': {
+                'title': 'Mean length in words of messages by Genre',
+                'yaxis': {
+                    'title': "Count"
+                },
+                'xaxis': {
+                    'title': "Genre"
+                }
+            }
+        }
+    ]
+    
+    
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+    ids_2 = ["graph-{}".format(i) for i, _ in enumerate(graphs_2)]
+    graphJSON2 = json.dumps(graphs_2, cls=plotly.utils.PlotlyJSONEncoder)
+   
     
     # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    return render_template('master.html', ids=ids, graphJSON=graphJSON, graphJSON2 = graphJSON2,ids_2=ids_2)
 
 
 # web page that handles user query and displays model results
